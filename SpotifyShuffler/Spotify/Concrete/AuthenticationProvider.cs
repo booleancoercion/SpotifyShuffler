31ce6d88f36efe9f3b44a41fb0d12097357f39fc
booleancoercion <booleancoercion@gmail.com>
2025-08-11 00:16:18 +0300

    AuthenticationProvider tests

--- SpotifyShuffler/Spotify/Concrete/AuthenticationProvider.cs
+++ SpotifyShuffler/Spotify/Concrete/AuthenticationProvider.cs
@@ -10,15 +10,18 @@ public class AuthenticationProvider : IAuthenticationProvider
     private readonly ILogger _logger;
     private readonly AuthenticationConfiguration _configuration;
     private readonly IApiWrapper _apiWrapper;
+    private readonly ICsrfStore _csrfStore;
 
     public AuthenticationProvider(
         ILogger<AuthenticationProvider> logger,
         AuthenticationConfiguration configuration,
-        IApiWrapper apiWrapper)
+        IApiWrapper apiWrapper,
+        ICsrfStore csrfStore)
     {
         _logger = logger;
         _configuration = configuration;
         _apiWrapper = apiWrapper;
+        _csrfStore = csrfStore;
     }
 
     public string GetUserAuthorizationUri(string? scope = null, bool showDialog = false)
@@ -28,10 +31,9 @@ public class AuthenticationProvider : IAuthenticationProvider
             {"client_id", _configuration.ClientId},
             {"response_type", "code"},
             {"redirect_uri", _configuration.RedirectUri},
-            {"show_dialog", showDialog.ToString().ToLower()}
+            {"show_dialog", showDialog.ToString().ToLower()},
+            {"state", _csrfStore.Generate()}
         };
-
-        qb.Add("state", "state");
 
         if (!string.IsNullOrEmpty(scope))
         {
